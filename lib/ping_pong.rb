91cc276f9d176478685eb6a49462eaf7b609ea85
Anna Horodetska and Ben Metzger <anna@>
2017-04-17 15:21:33 -0700

    add all day one projects

---
+++ lib/ping_pong.rb
@@ -0,0 +1,89 @@
+require('pry')
+class Fixnum
+  define_method(:ping_pong) do
+    pingpong_result = []
+    self.times() do |time|
+      pingpong_string = ""
+      start_at_one = time.+(1)
+      if start_at_one.%(3)==0
+        pingpong_string += "Ping"
+      end
+      if start_at_one.%(5)==0
+        pingpong_string += "Pong"
+      end
+      if pingpong_string.==("")
+        pingpong_string = start_at_one
+      end
+      pingpong_result.push(pingpong_string)
+      puts(pingpong_string)
+    end
+    pingpong_result
+  end
+end
+
+class String
+  define_method(:leet) do
+    empty_phrase = []
+    leet_letters = ["e", "o", "I", "s"]
+    split_phrase = self.split("")
+    split_phrase.each() do |letter|
+      if leet_letters.include?(letter)
+        new_letter = ""
+        if letter.==("e")
+          new_letter = "3"
+        elsif letter.==("o")
+          new_letter = "0"
+        elsif letter.==("I")
+          new_letter = "1"
+        elsif letter.==("s")
+          if empty_phrase.last().==(" ") || empty_phrase.==([])
+            new_letter = letter
+          else
+            new_letter = "z"
+          end
+        end
+      else
+        new_letter = letter
+      end
+      puts(new_letter)
+      empty_phrase.push(new_letter)
+    end
+    empty_phrase.join("")
+  end
+end
+
+class Array
+  define_method(:queen_attack?) do |coords|
+    if (self[0].-(coords[0])).abs() == (self[1].-(coords[1])).abs()
+      puts("diagonally")
+      true
+    elsif self[0].==(coords[0])
+      puts("vertical")
+      true
+    elsif self[1].==(coords[1])
+      puts("horizontal")
+      true
+    else
+      false
+    end
+  end
+end
+
+# [2,3] options
+# [7,8]
+#
+# 2-7 3-8
+# 5   5
+#
+# 0,5
+# 1,4
+#
+# 3,4
+# 4,5
+# 5,6
+#
+# 1,2
+# 0,1
+#
+# 3,2
+# 4,1
---
+++ spec/ping_pong_spec.rb
@@ -0,0 +1,68 @@
+require('rspec')
+require('ping_pong')
+
+describe('Fixnum#ping_pong') do
+  it("outputs 'Ping' if divisible by 3") do
+    expect(3.ping_pong()).to eq([1, 2, "Ping"])
+  end
+  it("outputs 'Pong' if divisible by 5") do
+    expect(5.ping_pong()).to eq([1, 2, "Ping", 4, "Pong"])
+  end
+  it("outputs 'PingPong' if divisible by 3") do
+    expect(15.ping_pong()).to eq([1, 2, "Ping", 4, "Pong", "Ping", 7, 8, "Ping", "Pong", 11, "Ping", 13, 14, "PingPong"])
+  end
+end
+
+describe('String#leet') do
+  it('replaces every "e" in a string with a "3"') do
+    expect("elephant".leet).to(eq("3l3phant"))
+  end
+
+ it('replaces every "o" in a string with a "0"') do
+    expect("boo boo".leet).to(eq("b00 b00"))
+  end
+
+ it('replaces every "I" in a string with a "1"') do
+    expect("I like Ike".leet).to(eq("1 lik3 1k3"))
+  end
+
+ it('replaces every "s" in a string with a "z"') do
+    expect("roses".leet).to(eq("r0z3z"))
+  end
+
+  it('does NOT replace the first letter when it is an s') do
+    expect("sassafrass".leet).to(eq("sazzafrazz"))
+  end
+
+  it('replaces letters correctly in a string of words') do
+    expect("I scream you scream we all scream for raspberry ice cream".leet).to(eq("1 scr3am y0u scr3am w3 all scr3am f0r razpb3rry ic3 cr3am"))
+  end
+end
+
+describe('Array#queen_attack?') do
+  it('is false if the coordinates are not horizontally, vertically, or diagonally in line with each other') do
+    expect([1,1].queen_attack?([2, 3])).to(eq(false))
+  end
+  it('is true if the coordinates are horizontally in line with each other') do
+    expect([1,1].queen_attack?([8,1])).to(eq(true))
+  end
+  it('is true if the coordinates are vertically in line with each other') do
+    expect([1,1].queen_attack?([1,5])).to(eq(true))
+  end
+  it('is true if the coordinates are diagonally in line with each other') do
+    expect([2,2].queen_attack?([6,6])).to(eq(true))
+  end
+  it('is true if the coordinates are diagonally in line with each other') do
+    expect([2,3].queen_attack?([1,4])).to(eq(true))
+  end
+  it('is true if the coordinates are diagonally in line with each other') do
+    expect([2,3].queen_attack?([3,4])).to(eq(true))
+  end
+  it('is true if the coordinates are diagonally in line with each other') do
+    expect([2,3].queen_attack?([1,2])).to(eq(true))
+  end
+  it('is true if the coordinates are diagonally in line with each other') do
+    expect([2,3].queen_attack?([3,2])).to(eq(true))
+  end
+  
+end
